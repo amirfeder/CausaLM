f761cadcaeb1d431ac009c94140d06838d47a26b
Nadav Oved <nadavo@gmail.com>
2019-11-14 21:06:41 +0200

    Fix bugs in pos_tagging.py script

--- datasets/pos_tagging.py
+++ datasets/pos_tagging.py
@@ -3,18 +3,21 @@ from xml.etree import ElementTree
 from BERT.constants import SENTIMENT_DATA_DIR
 from os.path import splitext
 from utils import init_logger
+from tqdm import tqdm
 import spacy
 import numpy as np
+import re
 
 ## MOVIES
 #TODO: Implement preprocessing (with POS tagging) for movies dataset
 #TODO: Create stratified train, dev and test sets for movies dataset
 
 ## DOMAINS
-#TODO: Fix _SPACE bug
 #TODO: Create stratified train, dev and test sets per domain and combined
 
-LOGGER = init_logger(__name__, __file__)
+LOGGER = init_logger(__name__)
+TOKEN_SEPARATOR = " "
+WORD_POS_SEPARATOR = "_"
 
 
 @timer(logger=LOGGER)
@@ -31,24 +34,25 @@ def main():
 def tag_xml_dataset(dataset_file, tagger):
     LOGGER.info(f"Tagging {dataset_file}")
     data_tree = ElementTree.parse(dataset_file)
-    data_root = data_tree.getroot()
     tagged_dataset = []
     num_adj = 0
     num_adv = 0
     num_words = 0
     review_lengths = []
-    for review in data_root:
-        review_text = review.text.strip()
+    for review in tqdm(data_tree.iter("review")):
+        review_text = re.sub("\n", "", review.text)
+        review_text = re.sub(";", ",", review_text)
+        review_text = re.sub("  ", " ", review_text).strip()
         tagged_review_text = []
         for token in tagger(review_text):
-            tagged_review_text.append(f"{token.text}_{token.pos_}")
+            tagged_review_text.append(f"{token.text}{WORD_POS_SEPARATOR}{token.pos_}")
             num_words += 1
             if token.pos_ == "ADJ":
                 num_adj += 1
             if token.pos_ == "ADV":
                 num_adv += 1
         review_lengths.append(len(tagged_review_text))
-        tagged_dataset.append(" ".join(tagged_review_text))
+        tagged_dataset.append(TOKEN_SEPARATOR.join(tagged_review_text))
     LOGGER.info(f"Number of reviews: {len(tagged_dataset)}")
     LOGGER.info(f"Number of words: {num_words}")
     LOGGER.info(f"Maximum number of words per review: {np.max(review_lengths)}")
@@ -61,9 +65,9 @@ def tag_xml_dataset(dataset_file, tagger):
     LOGGER.info(f"Total Adverbs ratio: {float(num_adv)/num_words}")
     LOGGER.info(f"Total Adverbs + Adjectives ratio: {float(num_adv + num_adj)/num_words}\n")
     _, file_ext = splitext(dataset_file)
-    tagged_dataset_file = f"{dataset_file.strip(file_ext)}_tagged{file_ext}"
+    tagged_dataset_file = f"{dataset_file.replace(file_ext, f'_tagged{file_ext}')}"
     with open(tagged_dataset_file, "w") as tagged_file:
-        tagged_file.writelines(tagged_dataset)
+        tagged_file.write("\n".join(tagged_dataset))
 
 
 if __name__ == "__main__":
