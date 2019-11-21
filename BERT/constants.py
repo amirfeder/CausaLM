06da982aed223544467a8784bf82600954cece0c
Nadav Oved <nadavo@gmail.com>
2019-11-21 21:40:10 +0200

    Implement training data generation for IMA task

--- BERT/constants.py
+++ BERT/constants.py
@@ -3,6 +3,7 @@ from os import getenv
 HOME_DIR = getenv('HOME', "/home/{}".format(getenv('USER', "/home/amirf")))
 DATA_DIR = f"{HOME_DIR}/GoogleDrive/AmirNadav/CausaLM/Data"
 SENTIMENT_DATA_DIR = f"{DATA_DIR}/Sentiment"
-IMA_DATA_DIR = f"{DATA_DIR}/Sentiment/iMA"
+IMA_DATA_DIR = f"{DATA_DIR}/Sentiment/IMA"
 SENTIMENT_DOMAINS = ["books", "electronics", "kitchen", "dvd"]
-BERT_PRETRAINED_MODEL = 'bert-base-uncased'
+BERT_PRETRAINED_MODEL = 'bert-base-cased'
+MAX_SEQ_LENGTH = 512
