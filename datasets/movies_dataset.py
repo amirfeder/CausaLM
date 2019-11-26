98f6034d3f861f97d3901ec61cc81868499a55ee
Nadav Oved <nadavo@gmail.com>
2019-11-26 13:17:00 +0200

    Fix movies_dataset script

--- datasets/movies_dataset.py
+++ datasets/movies_dataset.py
@@ -1,5 +1,6 @@
 from datasets.pos_tagging import clean_text, WORD_POS_SEPARATOR, TOKEN_SEPARATOR
 from constants import SENTIMENT_DATA_DIR
+from Timer import timer
 import pandas as pd
 import spacy
 
@@ -8,31 +9,31 @@ MOVIES_DATASET = f"{SENTIMENT_DATA_DIR}/movie_data"
 
 tagger = spacy.load("en_core_web_lg")
 
-tagged_dataset = []
 num_adj = 0
 num_adv = 0
 num_words = 0
 review_lengths = []
 df = pd.read_csv(MOVIES_DATASET + '.csv')
 
-output_datasets = {0:'negative', 1:'positive'}
-
-for key in output_datasets.keys():
-    cur_df = df[df['sentiment'] == key].reset_index()
-    for i in range(len(cur_df)):
-        review_text = clean_text(cur_df['review'][i])
-        tagged_review_text = []
-        for token in tagger(review_text):
-            tagged_review_text.append(f"{token.text}{WORD_POS_SEPARATOR}{token.pos_}")
-            num_words += 1
-            if token.pos_ == "ADJ":
-                num_adj += 1
-            if token.pos_ == "ADV":
-                num_adv += 1
-        review_lengths.append(len(tagged_review_text))
-        tagged_dataset.append(TOKEN_SEPARATOR.join(tagged_review_text))
+output_datasets = {0: 'negative', 1: 'positive'}
 
 
-    tagged_dataset_file = MOVIES_DATASET + '_' + output_datasets[key] + '.txt'
-    with open(tagged_dataset_file, "w") as tagged_file:
-        tagged_file.write("\n".join(tagged_dataset))
+def tag_review(review: str) -> str:
+    review_text = clean_text(review)
+    tagged_review = [f"{token.text}{WORD_POS_SEPARATOR}{token.pos_}" for token in tagger(review_text)]
+    return TOKEN_SEPARATOR.join(tagged_review)
+
+
+@timer
+def main():
+    for key in output_datasets.keys():
+        cur_df = df[df['sentiment'] == key].reset_index()
+        tagged_dataset = cur_df['review'].apply(tag_review)
+
+        tagged_dataset_file = MOVIES_DATASET + '_' + output_datasets[key] + '.txt'
+        with open(tagged_dataset_file, "w") as tagged_file:
+            tagged_file.write("\n".join(tagged_dataset))
+
+
+if __name__ == "__main__":
+    main()
