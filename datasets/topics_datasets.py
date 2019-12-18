009e0986b9456f5b177fd36852d86e86fbd23a0d
amirfeder <amirfeder@gmail.com>
2019-12-18 16:53:54 +0200

    Topic Dataset initial commit.

---
+++ datasets/topics_datasets.py
@@ -0,0 +1,75 @@
+"""Create train/dev/test data with and without adjectives"""
+from constants import SENTIMENT_RAW_DATA_DIR, SENTIMENT_DOMAINS
+from datasets.datasets_utils import output_datasets
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer
+import os
+import xml.etree.ElementTree as ET
+
+## Model Hyperparams
+num_topics = 30
+num_features = 500
+num_top_words = 10
+
+def XML2arrayRAW(cur_path):
+    reviews = []
+    tree = ET.parse(cur_path)
+    root = tree.getroot()
+    for rev in root.iter('review'):
+        reviews.append(rev.text)
+    return reviews
+
+
+def display_topics(model, feature_names, no_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print("\n Topic {}:".format(topic_idx))
+        print(" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]]))
+
+
+def get_topic_distribution(data):
+    tf_vectorizer = CountVectorizer(max_features=num_features, stop_words='english')
+    tf = tf_vectorizer.fit_transform(data)
+    tf_feature_names = tf_vectorizer.get_feature_names()
+
+    lda = LatentDirichletAllocation(n_components=num_topics, max_iter=10, learning_method='online',
+                                    learning_offset=50., random_state=0).fit(tf)
+
+    # display_topics(lda, tf_feature_names, num_top_words)
+    return lda.transform(tf)
+
+
+
+for domain in SENTIMENT_DOMAINS:
+    domain_data = []
+    domain_labels = []
+    examples = []
+    DOMAIN_DIR = os.path.join(SENTIMENT_RAW_DATA_DIR, domain)
+
+    for key in output_datasets.keys():
+        DATA_FILE = os.path.join(DOMAIN_DIR, output_datasets[key] + '.parsed')
+        cur_data = XML2arrayRAW(DATA_FILE)
+
+        domain_data += cur_data
+        domain_labels += [key] * len(cur_data)
+
+    domain_topic_dist = get_topic_distribution(domain_data)
+    domain_data = list(zip(domain_labels, domain_data))
+    domain_data = np.array(domain_data)
+    zipped_domain = np.append(domain_data, domain_topic_dist, axis = 1)
+
+
+    domain_cols = ['topic_' + str(i+1) for i in range(num_topics)]
+    cols = ['label', 'review'] + domain_cols
+    df = pd.DataFrame(zipped_domain, columns=cols)
+
+    train, test = train_test_split(df, test_size=0.2)
+    train, dev = train_test_split(train, test_size=0.2)
+    train.to_csv(SENTIMENT_RAW_DATA_DIR + '/' + domain + '/' + 'topics_train.csv')
+    dev.to_csv(SENTIMENT_RAW_DATA_DIR + '/' + domain + '/' + 'topics_dev.csv')
+    test.to_csv(SENTIMENT_RAW_DATA_DIR + '/' + domain + '/' + 'topics_test.csv')
+
+    print('Done with: ' + domain)
+
