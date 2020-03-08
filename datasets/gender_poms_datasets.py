089df3834494ff82ed7846016d80d979a0f78274
Nadav Oved <nadavo@gmail.com>
2020-03-08 21:48:12 +0200

    Minor fixes to gender dataset scripts

--- datasets/gender_poms_datasets.py
+++ datasets/gender_poms_datasets.py
@@ -1,9 +1,11 @@
-from constants import POMS_GENDER_DATA_DIR, POMS_RAW_DATA_DIR
+from constants import POMS_GENDER_DATA_DIR, POMS_RAW_DATA_DIR, RANDOM_SEED
 from datasets_utils import split_data, TOKEN_SEPARATOR
+from Timer import timer
 import pandas as pd
 import numpy as np
 
 
+@timer
 def create_gender_datasets():
     df = pd.read_csv(f"{POMS_RAW_DATA_DIR}/Equity-Evaluation-Corpus.csv", header=0,
                      converters={"ID": lambda i: int(i.split("-")[-1])})
@@ -18,7 +20,7 @@ def create_gender_datasets():
     df_joined["label"] = df_joined["Emotion"].apply(lambda label: emotion_labels_dict.get(str(label), 0))
     df_joined = df_joined[["ID_f", "ID_m", "Person_f", "Person_m", "Sentence_f", "Sentence_m", "Template", "Race", "Emotion word", "label"]]
     print(df_joined)
-    df_joined_grouped = df_joined.groupby(by="ID_f", as_index=False).first().set_index(keys=["ID_f", "ID_m"]).sort_index()
+    df_joined_grouped = df_joined.groupby(by="ID_f", as_index=False).apply(lambda group: group.sample(n=1)).set_index(keys=["ID_f", "ID_m"]).sort_index()
     print(df_joined_grouped)
     sequence_lengths = df_joined_grouped['Sentence_f'].apply(lambda text: int(len(text.split(TOKEN_SEPARATOR))))
     print(f"Max sequence length in dataset: {np.max(sequence_lengths)}")
