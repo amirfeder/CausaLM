24a7641015aab7b75ed14f3717295916f3f28521
Nadav Oved <nadavo@gmail.com>
2020-04-07 00:51:37 +0300

    Implement GoogleDriveHandler

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from os import getenv
 from smtplib import SMTP
 from os import environ
 from email.message import EmailMessage
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, run
 from multiprocessing import cpu_count
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -165,3 +165,27 @@ def save_predictions(folder, sample_idx_list, predictions_list, true_list, corre
     df = pd.DataFrame.from_dict(df_dict)
     df = df.set_index("sample_index").sort_index()
     df.to_csv(f"{folder}/{name}-predictions.csv")
+
+
+class GoogleDriveHandler:
+    def __init__(self,
+                 local_root: str = f"{HOME_DIR}/GoogleDrive",
+                 drive_binary: str = f"{HOME_DIR}/bin/go/packages/bin/drive",
+                 default_timeout: int = 600):
+        self.local_root = local_root
+        self.drive_binary = drive_binary
+        self.default_args = ["-no-prompt"]
+        self.default_timeout = default_timeout
+
+    def _execute_drive_cmd(self, subcommand: str, path: str, cmd_args: list):
+        if subcommand not in ("pull", "push"):
+            raise ValueError("Only pull and push commands are currently supported")
+        cmd = [self.drive_binary, subcommand] + self.default_args + cmd_args + [path]
+        cmd_return = run(cmd, capture_output=True, text=True, timeout=self.default_timeout, cwd=HOME_DIR)
+        return cmd_return.returncode, cmd_return.stdout, cmd_return.stderr
+
+    def push_files(self, path: str, cmd_args: list = []):
+        return self._execute_drive_cmd("push", path, ["-files"] + cmd_args)
+
+    def pull_files(self, path: str, cmd_args: list = []):
+        return self._execute_drive_cmd("pull", path, ["-files"] + cmd_args)
